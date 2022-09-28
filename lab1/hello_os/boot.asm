a3acd2c872f2949da65088779edc9400338b30b9
SUNLIFAN <1656680420@qq.com>
2022-09-28 12:58:17 +0800

    add comment

--- lab1/hello_os/boot.asm
+++ lab1/hello_os/boot.asm
@@ -1,19 +1,19 @@
-	org 07c00h
-	mov ax, cs
+	org 07c00h ; load program at 0x7c00
+	mov ax, cs ; set seg reg
 	mov ds, ax
 	mov es, ax
-	call DispStr
+	call DispStr ; call display string
 	jmp $
 
 DispStr:
-	mov ax, BootMessage
-	mov bp, ax
-	mov cx, 16
+	mov ax, BootMessage 
+	mov bp, ax ; es:bp = string address
+	mov cx, 16 ; string length
 	mov ax, 01301h
 	mov bx, 000ch
 	mov dl, 0
 	int 10h
 	ret
 BootMessage: db "Hello, OS world!"
-times 510-($-$$) db 0
-dw 0xaa55
+times 510-($-$$) db 0 ; padding to 512 bytes
+dw 0xaa55 ; end flag

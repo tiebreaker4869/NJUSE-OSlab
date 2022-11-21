2e7cd8282b5fc779fce297d65f04608b9c6a83d7
SUNLIFAN <lifansun1412@gmail.com>
2022-11-21 09:25:01 +0800

    fix def

--- lab3/include/console.h
+++ lab3/include/console.h
@@ -8,6 +8,15 @@
 #ifndef _ORANGES_CONSOLE_H_
 #define _ORANGES_CONSOLE_H_
 
+#define SCR_UP	1	/* scroll forward */
+#define SCR_DN	-1	/* scroll backward */
+
+#define SCREEN_SIZE		(80 * 25)
+#define SCREEN_WIDTH		80
+#define TAB_WIDTH 4
+
+#define DEFAULT_CHAR_COLOR	0x07	/* 0000 0111 黑底白字 */
+
 typedef struct pos_stack {
 	unsigned int esp;
 	unsigned int pos[SCREEN_SIZE];
@@ -23,14 +32,8 @@ typedef struct s_console
 	STACK backtrace_stack;          /* 用来回退的栈 */
 }CONSOLE;
 
-#define SCR_UP	1	/* scroll forward */
-#define SCR_DN	-1	/* scroll backward */
 
-#define SCREEN_SIZE		(80 * 25)
-#define SCREEN_WIDTH		80
-#define TAB_WIDTH 4
 
-#define DEFAULT_CHAR_COLOR	0x07	/* 0000 0111 黑底白字 */
 
 
 #endif /* _ORANGES_CONSOLE_H_ */

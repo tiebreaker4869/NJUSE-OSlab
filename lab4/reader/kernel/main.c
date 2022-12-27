85faae4be5ab48acef64cae5d568009995f832f3
SUNLIFAN <lifansun1412@gmail.com>
2022-12-27 21:37:52 +0800

    test output

--- lab4/reader/kernel/main.c
+++ lab4/reader/kernel/main.c
@@ -158,6 +158,12 @@ void F()
 
         myprint(s);
 
+        for (int i = 0; i < NR_TASKS - 1; i ++) {
+            PROCESS* cur_process = proc_table + i;
+            char ptype[3] = {cur_process->type, , ' ', '\0'};
+            print(ptype);
+        }
+
         myprint("\n");
 
         print_index ++;

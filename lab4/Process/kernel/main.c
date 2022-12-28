8a11330c67de84dae99bd8904f16d5e082f35e88
SUNLIFAN <lifansun1412@gmail.com>
2022-12-28 10:35:52 +0800

    fix

--- lab4/Process/kernel/main.c
+++ lab4/Process/kernel/main.c
@@ -13,288 +13,288 @@
 #include "proc.h"
 #include "global.h"
 
-SEMAPHORE r; 
-SEMAPHORE w; 
+SEMAPHORE r;
+SEMAPHORE w;
 SEMAPHORE mutex;
 
-SEMAPHORE r_mutex; int r_count;
-SEMAPHORE w_mutex; int w_count;
+SEMAPHORE r_mutex;
+int r_count;
+SEMAPHORE w_mutex;
+int w_count;
 
 /*======================================================================*
                             kernel_main
  *======================================================================*/
-PUBLIC int kernel_main()
-{
-	disp_str("-----\"kernel_main\" begins-----\n");
-	
-	TASK*		p_task		= task_table;
-	PROCESS*	p_proc		= proc_table;
-	char*		p_task_stack	= task_stack + STACK_SIZE_TOTAL;
-	u16		selector_ldt	= SELECTOR_LDT_FIRST;
-	int i;
-	for (i = 0; i < NR_TASKS; i++) {
-		strcpy(p_proc->p_name, p_task->name);	// name of the process
-		p_proc->pid = i;			// pid
+PUBLIC int kernel_main() {
+    disp_str("-----\"kernel_main\" begins-----\n");
 
-		p_proc->ticks = 0;
-		p_proc->delay_ticks = 0;
-		p_proc->inqueue = 0;
+    TASK *p_task = task_table;
+    PROCESS *p_proc = proc_table;
+    char *p_task_stack = task_stack + STACK_SIZE_TOTAL;
+    u16 selector_ldt = SELECTOR_LDT_FIRST;
+    int i;
+    for (i = 0; i < NR_TASKS; i++) {
+        strcpy(p_proc->p_name, p_task->name);    // name of the process
+        p_proc->pid = i;            // pid
 
-		p_proc->ldt_sel = selector_ldt;
+        p_proc->ticks = 0;
+        p_proc->delay_ticks = 0;
+        p_proc->inqueue = 0;
 
-		memcpy(&p_proc->ldts[0], &gdt[SELECTOR_KERNEL_CS >> 3],
-		       sizeof(DESCRIPTOR));
-		p_proc->ldts[0].attr1 = DA_C | PRIVILEGE_TASK << 5;
-		memcpy(&p_proc->ldts[1], &gdt[SELECTOR_KERNEL_DS >> 3],
-		       sizeof(DESCRIPTOR));
-		p_proc->ldts[1].attr1 = DA_DRW | PRIVILEGE_TASK << 5;
-		p_proc->regs.cs	= ((8 * 0) & SA_RPL_MASK & SA_TI_MASK)
-			| SA_TIL | RPL_TASK;
-		p_proc->regs.ds	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
-			| SA_TIL | RPL_TASK;
-		p_proc->regs.es	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
-			| SA_TIL | RPL_TASK;
-		p_proc->regs.fs	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
-			| SA_TIL | RPL_TASK;
-		p_proc->regs.ss	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
-			| SA_TIL | RPL_TASK;
-		p_proc->regs.gs	= (SELECTOR_KERNEL_GS & SA_RPL_MASK)
-			| RPL_TASK;
+        p_proc->ldt_sel = selector_ldt;
 
-		p_proc->regs.eip = (u32)p_task->initial_eip;
-		p_proc->regs.esp = (u32)p_task_stack;
-		p_proc->regs.eflags = 0x1202; /* IF=1, IOPL=1 */
+        memcpy(&p_proc->ldts[0], &gdt[SELECTOR_KERNEL_CS >> 3],
+               sizeof(DESCRIPTOR));
+        p_proc->ldts[0].attr1 = DA_C | PRIVILEGE_TASK << 5;
+        memcpy(&p_proc->ldts[1], &gdt[SELECTOR_KERNEL_DS >> 3],
+               sizeof(DESCRIPTOR));
+        p_proc->ldts[1].attr1 = DA_DRW | PRIVILEGE_TASK << 5;
+        p_proc->regs.cs = ((8 * 0) & SA_RPL_MASK & SA_TI_MASK)
+                          | SA_TIL | RPL_TASK;
+        p_proc->regs.ds = ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+                          | SA_TIL | RPL_TASK;
+        p_proc->regs.es = ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+                          | SA_TIL | RPL_TASK;
+        p_proc->regs.fs = ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+                          | SA_TIL | RPL_TASK;
+        p_proc->regs.ss = ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+                          | SA_TIL | RPL_TASK;
+        p_proc->regs.gs = (SELECTOR_KERNEL_GS & SA_RPL_MASK)
+                          | RPL_TASK;
 
-		p_task_stack -= p_task->stacksize;
-		p_proc++;
-		p_task++;
-		selector_ldt += 1 << 3;
-	}
+        p_proc->regs.eip = (u32) p_task->initial_eip;
+        p_proc->regs.esp = (u32) p_task_stack;
+        p_proc->regs.eflags = 0x1202; /* IF=1, IOPL=1 */
 
-	k_reenter = 0;
-	ticks = 0;
+        p_task_stack -= p_task->stacksize;
+        p_proc++;
+        p_task++;
+        selector_ldt += 1 << 3;
+    }
 
-	p_proc_ready	= proc_table+NR_TASKS-1;
+    k_reenter = 0;
+    ticks = 0;
+
+    p_proc_ready = proc_table + NR_TASKS - 1;
 
     // 初始化任务状态
-    for (int i = 0; i < NR_TASKS - 1; i ++) {
+    for (int i = 0; i < NR_TASKS - 1; i++) {
         task_status[i] = 2;
     }
 
     print_index = 1;
 
-	init_sema(&r, READERS);
-	init_sema(&w, WRITERS);
-	init_sema(&mutex, MUTEX);
-	init_sema(&r_mutex, MUTEX); r_count = 0;
-	init_sema(&w_mutex, MUTEX); w_count = 0;
+    init_sema(&r, READERS);
+    init_sema(&w, WRITERS);
+    init_sema(&mutex, MUTEX);
+    init_sema(&r_mutex, MUTEX);
+    r_count = 0;
+    init_sema(&w_mutex, MUTEX);
+    w_count = 0;
 
-	init_queue();
-	init_clock();
-	clear();
+    init_queue();
+    init_clock();
+    clear();
 
-	restart();
+    restart();
 
-	while(1){}
+    while (1) {}
 }
 
-void business(int task_time){
-	milli_delay(task_time * TIME_SLICE);
+void business(int task_time) {
+    milli_delay(task_time * TIME_SLICE);
 }
 
-void FAIR_R(char* s, int task_time){
+void FAIR_R(char *s, int task_time) {
     int current_index = s[0] - 'A';
-	while (1) {
-        task_status[current_index] = 0;
-		P(&mutex);
-
-			P(&r);
-
-				P(&r_mutex);
-				if(r_count==0) P(&w);
-				r_count++;
-				V(&r_mutex);
-
-		V(&mutex);
-            task_status[current_index] = 1;
-			business(task_time);
-			
-				P(&r_mutex);
-				r_count--;
-				if(r_count==0) V(&w);
-				V(&r_mutex);
-			
-			V(&r);
-            task_status[current_index] = 2;
-
-            mydelay(TIME_SLICE);
-	}
-}
-void FAIR_W(char* s, int task_time){
-	int current_index = s[0] - 'A';
     while (1) {
         task_status[current_index] = 0;
-		P(&mutex);
+        P(&mutex);
 
-			P(&w);
+        P(&r);
 
-		V(&mutex);
-            task_status[current_index] = 1;
-			business(task_time);
-			V(&w);
-            task_status[current_index] = 2;
-            mydelay(TIME_SLICE);
-	}
+        P(&r_mutex);
+        if (r_count == 0) P(&w);
+        r_count++;
+        V(&r_mutex);
+
+        V(&mutex);
+        task_status[current_index] = 1;
+        business(task_time);
+
+        P(&r_mutex);
+        r_count--;
+        if (r_count == 0) V(&w);
+        V(&r_mutex);
+
+        V(&r);
+        task_status[current_index] = 2;
+
+        mydelay(TIME_SLICE);
+    }
 }
 
-void READER_FIRST_R(char* s, int task_time){
-	while (1) {	
-		P(&r);
-		
-			P(&r_mutex);
-			if(r_count==0) P(&w);
-			r_count++;
-			V(&r_mutex);
-		
-		business(task_time);
-		
-			P(&r_mutex);
-			r_count--;
-			if(r_count==0) V(&w);
-			V(&r_mutex);
-		
-		V(&r);
-		
-		// avoid straving
-		mydelay(task_time/HZ*T/2);
-	}
-}
-void READER_FIRST_W(char* s, int task_time){
-	while (1) {		
-		P(&w);
-		business(task_time);
-		V(&w);
-	}
+void FAIR_W(char *s, int task_time) {
+    int current_index = s[0] - 'A';
+    while (1) {
+        task_status[current_index] = 0;
+        P(&mutex);
+
+        P(&w);
+
+        V(&mutex);
+        task_status[current_index] = 1;
+        business(task_time);
+        V(&w);
+        task_status[current_index] = 2;
+        mydelay(TIME_SLICE);
+    }
 }
 
-void WRITER_FIRST_R(char* s, int task_time){
-	while (1) {	
-		P(&r);
+void READER_FIRST_R(char *s, int task_time) {
+    while (1) {
+        P(&r);
 
-			P(&mutex);
-				P(&r_mutex);
-				if(r_count==0) P(&w);
-				r_count++;
-				V(&r_mutex);
-			V(&mutex);
+        P(&r_mutex);
+        if (r_count == 0) P(&w);
+        r_count++;
+        V(&r_mutex);
 
-		business(task_time);
-		
-				P(&r_mutex);
-				r_count--;
-				if(r_count==0) V(&w);
-				V(&r_mutex);
-		
-		V(&r);
-	}
-}
-void WRITER_FIRST_W(char* s, int task_time){
-	while (1) {
-		P(&w_mutex);
-		if(w_count==0)	P(&mutex);
-		w_count++;
-		V(&w_mutex);
+        business(task_time);
 
-		P(&w);
-		business(task_time);
-		V(&w);
+        P(&r_mutex);
+        r_count--;
+        if (r_count == 0) V(&w);
+        V(&r_mutex);
 
-		P(&w_mutex);
-		w_count--;
-		if(w_count==0)	V(&mutex);
-		V(&w_mutex);
+        V(&r);
 
-		// avoid straving
-		mydelay(task_time/HZ*T);
-	}
+        // avoid straving
+        mydelay(task_time / HZ * T / 2);
+    }
 }
 
-void(* R_MODE[MODE_COUNT])(char*, int) = {FAIR_R, READER_FIRST_R, WRITER_FIRST_R};
-void(* W_MODE[MODE_COUNT])(char*, int) = {FAIR_W, READER_FIRST_W, WRITER_FIRST_W};
+void READER_FIRST_W(char *s, int task_time) {
+    while (1) {
+        P(&w);
+        business(task_time);
+        V(&w);
+    }
+}
+
+void WRITER_FIRST_R(char *s, int task_time) {
+    while (1) {
+        P(&r);
+
+        P(&mutex);
+        P(&r_mutex);
+        if (r_count == 0) P(&w);
+        r_count++;
+        V(&r_mutex);
+        V(&mutex);
+
+        business(task_time);
+
+        P(&r_mutex);
+        r_count--;
+        if (r_count == 0) V(&w);
+        V(&r_mutex);
+
+        V(&r);
+    }
+}
+
+void WRITER_FIRST_W(char *s, int task_time) {
+    while (1) {
+        P(&w_mutex);
+        if (w_count == 0) P(&mutex);
+        w_count++;
+        V(&w_mutex);
+
+        P(&w);
+        business(task_time);
+        V(&w);
+
+        P(&w_mutex);
+        w_count--;
+        if (w_count == 0) V(&mutex);
+        V(&w_mutex);
+
+        // avoid straving
+        mydelay(task_time / HZ * T);
+    }
+}
+
+void (*R_MODE[MODE_COUNT])(char *, int) = {FAIR_R, READER_FIRST_R, WRITER_FIRST_R};
+
+void (*W_MODE[MODE_COUNT])(char *, int) = {FAIR_W, READER_FIRST_W, WRITER_FIRST_W};
 
 /*======================================================================*
                                TestA
  *======================================================================*/
-void TestA()
-{
-	int tt = 2;
-	R_MODE[MODE]("A", tt);
+void TestA() {
+    int tt = 2;
+    R_MODE[MODE]("A", tt);
 }
 
 /*======================================================================*
                                TestB
  *======================================================================*/
-void TestB()
-{
-	int tt = 3;
-	R_MODE[MODE]("B", tt);
+void TestB() {
+    int tt = 3;
+    R_MODE[MODE]("B", tt);
 }
 
 /*======================================================================*
                                TestC
  *======================================================================*/
-void TestC()
-{
-	int tt = 3;
-	R_MODE[MODE]("C", tt);
+void TestC() {
+    int tt = 3;
+    R_MODE[MODE]("C", tt);
 }
 
 /*======================================================================*
                                TestD
  *======================================================================*/
-void TestD()
-{
-	int tt = 3;
-	W_MODE[MODE]("D", tt);
+void TestD() {
+    int tt = 3;
+    W_MODE[MODE]("D", tt);
 }
 
 /*======================================================================*
                                TestE
  *======================================================================*/
-void TestE()
-{
-	int tt = 4;
-	W_MODE[MODE]("E", tt);
+void TestE() {
+    int tt = 4;
+    W_MODE[MODE]("E", tt);
 }
 
 /*======================================================================*
                                TestF
  *======================================================================*/
 
-void TestF()
-{
-	int tt = 1;
+void TestF() {
+    int tt = 1;
 
     while (1) {
-        char* s;
+        char *s;
         if (print_index > 20) {
             mydelay(tt * TIME_SLICE);
+            continue;
+        }
+        if (print_index < 10) {
+            char index[2] = {print_index + '0', '\0'};
+            s = index;
         } else {
-            if (print_index < 10) {
-                char index[2] = {print_index + '0', '\0'};
-                s = index;
-            } else {
-                char index[3] = {print_index / 10 + '0', print_index % 10 + '0', '\0'};
-                s = index;
-            }
+            char index[3] = {print_index / 10 + '0', print_index % 10 + '0', '\0'};
+            s = index;
         }
 
         print(s);
 
         print(" ");
 
-        print_index ++;
-        for (int i = 0; i < NR_TASKS-1; i ++) {
+        for (int i = 0; i < NR_TASKS - 1; i++) {
             switch (task_status[i]) {
                 case 0:
                     print("X");
@@ -311,6 +311,7 @@ void TestF()
             }
         }
         print("\n");
+        print_index++;
         mydelay(tt * TIME_SLICE);
-	}
+    }
 }
